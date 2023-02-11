687564c38e8e2b7d3099e55dadec89b1b24e783c
Maciej Zielonka <mapazi@op.pl>
2023-02-11 22:13:51 +0100

    feat: add editable position

--- TargetView.js
+++ TargetView.js
@@ -9,6 +9,8 @@ class TargetView
         this.YOffset = 0;
         this.Width = 0;
         this.Height = 0;
+        this.CenterX = 0;
+        this.CenterY = 0;
     }
 
     draw(xOffset, yOffset, width, height)
@@ -19,37 +21,39 @@ class TargetView
         this.Height = height;
         var targetHeightPercentage = 0.85;
         this.LocalScale = this.Height / this.TargetDimensions.bigDiameter * targetHeightPercentage;
+        this.CenterX = this.XOffset + this.Width * 0.5;
+        this.CenterY = this.YOffset + this.Height * 0.5;
         strokeWeight(3);
         var targetDimensionsVM = new TargetDimsVM(this.LocalScale, this.TargetDimensions);
         stroke(0);
-        this.drawCircle(this.Width * 0.5, this.Height * 0.5, targetDimensionsVM.bigDiameter);
-        this.drawCircle(this.Width * 0.5, this.Height * 0.5, targetDimensionsVM.bigDiameter - 2 * targetDimensionsVM.doubleMultiplierWidth);
-        this.drawCircle(this.Width * 0.5, this.Height * 0.5, targetDimensionsVM.mediumDiameter);
-        this.drawCircle(this.Width * 0.5, this.Height * 0.5, targetDimensionsVM.mediumDiameter - 2 * targetDimensionsVM.trippleMultiplierWidth);
+        this.drawCircle(this.CenterX, this.CenterY, targetDimensionsVM.bigDiameter);
+        this.drawCircle(this.CenterX, this.CenterY, targetDimensionsVM.bigDiameter - 2 * targetDimensionsVM.doubleMultiplierWidth);
+        this.drawCircle(this.CenterX, this.CenterY, targetDimensionsVM.mediumDiameter);
+        this.drawCircle(this.CenterX, this.CenterY, targetDimensionsVM.mediumDiameter - 2 * targetDimensionsVM.trippleMultiplierWidth);
         var angleStep = 360 / 20;
         var startAngle = angleStep / 2;
         for (let i = 0; i < 20; i++)
         {
             var angle = (radians(startAngle + i * angleStep));
-            line(this.Width * 0.5 - sin(angle) * targetDimensionsVM.bigDiameter / 2,
-                this.Height * 0.5 - cos(angle) * targetDimensionsVM.bigDiameter / 2,
-                this.Width * 0.5 + sin(angle) * targetDimensionsVM.bigDiameter / 2,
-                this.Height * 0.5 + cos(angle) * targetDimensionsVM.bigDiameter / 2);
+            line(this.CenterX - sin(angle) * targetDimensionsVM.bigDiameter / 2,
+                this.CenterY - cos(angle) * targetDimensionsVM.bigDiameter / 2,
+                this.CenterX + sin(angle) * targetDimensionsVM.bigDiameter / 2,
+                this.CenterY + cos(angle) * targetDimensionsVM.bigDiameter / 2);
         }
-        this.drawCircle(this.Width * 0.5, this.Height * 0.5, targetDimensionsVM.smallDiameter);
-        this.drawCircle(this.Width * 0.5, this.Height * 0.5, targetDimensionsVM.smallMultiplierDiameter);
+        this.drawCircle(this.CenterX, this.CenterY, targetDimensionsVM.smallDiameter);
+        this.drawCircle(this.CenterX, this.CenterY, targetDimensionsVM.smallMultiplierDiameter);
         var labelHeightPercentage = 1 - targetHeightPercentage;
         var marginPercentage = 0.4; //percentage of the margin in text label
         var textHeightPercentage = labelHeightPercentage - (labelHeightPercentage * marginPercentage);
         var textHeight = this.Height * textHeightPercentage * 0.66;
         var marginHeight = this.Height * labelHeightPercentage * marginPercentage / 2;
-        this.drawNumbers(this.Width * 0.5, this.Height * 0.5, targetDimensionsVM.bigDiameter * 0.5 + marginHeight / 2, textHeight, this.ScoringInfo);
+        this.drawNumbers(this.CenterX, this.CenterY, targetDimensionsVM.bigDiameter * 0.5 + marginHeight / 2, textHeight, this.ScoringInfo);
     }
 
     getRelativeScoreBoardMousePosition()
     {
-        var mappedX = (mouseX - width * 0.5) / this.LocalScale;
-        var mappedY = (-mouseY + this.Height * 0.5) / this.LocalScale;
+        var mappedX = (mouseX - this.CenterX) / this.LocalScale;
+        var mappedY = (-mouseY + this.CenterY) / this.LocalScale;
         return [mappedX, mappedY];
     }
 
